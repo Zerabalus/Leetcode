9abaeb8648740381d71fa2a7065f305b4d9e32b3
Epharedam <129038355+Epharedam@users.noreply.github.com>
2025-02-15 00:12:41 -0600

    2698. Find the Punishment Number of an Integer

--- Medium/PunishmentNumberOfAnInteger.java
+++ Medium/PunishmentNumberOfAnInteger.java
@@ -8,19 +8,24 @@ The punishment number of n is defined as the sum of the squares of all integers 
     The decimal representation of i * i can be partitioned into contiguous substrings such that the sum of the integer values of these substrings equals i.
  */
 
+/*  Dado un número nn, queremos encontrar la suma de los cuadrados de ciertos números desde 11 hasta nn, pero solo de aquellos números que cumplen una condición especial.
+Condición especial:
+
+Si tomamos el cuadrado de un número, debe ser posible dividir sus dígitos en segmentos cuya suma sea igual al número original. */
+
 public class PunishmentNumberOfAnInteger {
     public int punishmentNumber(int n) {
         // para almacenar el numero
         int punishmentNum = 0;
 
         // Recorremos los números del 1 hasta n
-        for (int curr = 1; curr <= n; curr++) {
+        for (int testNumber = 1; testNumber <= n; testNumber++) {
 
             // encontrar el cuadrado
-            int square = curr * curr;
+            int square = testNumber * testNumber;
 
             // Si es válido, sumamos el cuadrado al resultado final
-            if (canPartition(square, curr))
+            if (canPartition(square, testNumber))
                 punishmentNum += square;
         }
 
@@ -40,7 +45,7 @@ public class PunishmentNumberOfAnInteger {
 
         // Intentamos dividir el número en diferentes segmentos y verificamos si alguno
         // funciona
-        return (canPartition(num / 10, target - (num % 10)) ||  // Tomamos el último dígito
+        return (canPartition(num / 10, target - (num % 10)) || // Tomamos el último dígito
                 canPartition(num / 100, target - (num % 100)) || // Tomamos los últimos dos dígitos
                 canPartition(num / 1000, target - (num % 1000))); // Tomamos los últimos tres dígitos
     }
